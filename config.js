cf3d1388fdc888a4e0f5f2b077ca5b900f104047
Ben Terrell <bterrell71@gmail.com>
2017-06-09 16:34:50 -0400

    updated config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+//globals: equal, responseText, statement, ok, deepEqual, QUnit, module, asyncTest, Util, start, golfStatements, console
+/*jslint bitwise: true, browser: true, plusplus: true, maxerr: 50, indent: 4 */
+function Config() {
+    "use strict";
+}
+//Config.endpoint = "<TCAPI end point (ending with slash)>";
+//Config.authUser = "<account id>";
+//Config.authPassword = "<password>";
+//Config.actor = { "mbox":"<learner email>", "name":"<Learner Name>" };
+//Config.registration = "<registration-uuid>";
+
+Config.endpoint = "https://sandbox.watershedlrs.com/api/organizations/4781/";
+Config.authUser = "778f48b68331df";
+Config.authPassword = "a14a9bf93e57a3";
+Config.actor = { "mbox":"ben.terrell@verizonwireless.com", "name":"Ben Terrell" };
+Config.registration = "<registration-uuid>";
