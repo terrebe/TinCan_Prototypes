134c9a8a85ec9c6857425b7499278bb0bd49a563
Ben Terrell <bterrell71@gmail.com>
2017-06-09 16:50:20 -0400

    updated config.js to remove name

--- config.js
+++ config.js
@@ -12,5 +12,5 @@ function Config() {
 Config.endpoint = "https://sandbox.watershedlrs.com/api/organizations/4781/";
 Config.authUser = "778f48b68331df";
 Config.authPassword = "a14a9bf93e57a3";
-Config.actor = { "mbox":"ben.terrell@verizonwireless.com", "name":"Ben Terrell" };
+Config.actor = { "mbox":"<learner email>", "name":"<Learner Name>" };
 Config.registration = "<registration-uuid>";
